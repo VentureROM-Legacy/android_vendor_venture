e649999815667aa854484826e8f8cf5545c6e8f5
Vedant <vedant@mathur.ca>
2014-09-12 20:55:56 -0700

    [1/2] bringup

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -12,222 +12,222 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq (pa_a510,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_a510.mk
+ifeq (venture_a510,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_a510.mk
 endif
-ifeq (pa_a700,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_a700.mk
+ifeq (venture_a700,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_a700.mk
 endif
-ifeq (pa_amami,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_amami.mk
+ifeq (venture_amami,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_amami.mk
 endif
-ifeq (pa_d2lte,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_d2lte.mk
+ifeq (venture_d2lte,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_d2lte.mk
 endif
-ifeq (pa_bacon,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_bacon.mk
+ifeq (venture_bacon,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_bacon.mk
 endif
-ifeq (pa_d800,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_d800.mk
+ifeq (venture_d800,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_d800.mk
 endif
-ifeq (pa_d801,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_d801.mk
+ifeq (venture_d801,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_d801.mk
 endif
-ifeq (pa_d802,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_d802.mk
+ifeq (venture_d802,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_d802.mk
 endif
-ifeq (pa_d803,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_d803.mk
+ifeq (venture_d803,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_d803.mk
 endif
-ifeq (pa_dlx,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_dlx.mk
+ifeq (venture_dlx,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_dlx.mk
 endif
-ifeq (pa_edison,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_edison.mk
+ifeq (venture_edison,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_edison.mk
 endif
-ifeq (pa_enrc2b,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_enrc2b.mk
+ifeq (venture_enrc2b,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_enrc2b.mk
 endif
-ifeq (pa_evita,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_evita.mk
+ifeq (venture_evita,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_evita.mk
 endif
-ifeq (pa_falcon,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_falcon.mk
+ifeq (venture_falcon,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_falcon.mk
 endif
-ifeq (pa_find5,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_find5.mk
+ifeq (venture_find5,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_find5.mk
 endif
-ifeq (pa_find7,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_find7.mk
+ifeq (venture_find7,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_find7.mk
 endif
-ifeq (pa_find7u,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_find7u.mk
+ifeq (venture_find7u,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_find7u.mk
 endif
-ifeq (pa_flo,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_flo.mk
+ifeq (venture_flo,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_flo.mk
 endif
-ifeq (pa_galaxysmtd,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_galaxysmtd.mk
+ifeq (venture_galaxysmtd,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_galaxysmtd.mk
 endif
-ifeq (pa_gee,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_gee.mk
+ifeq (venture_gee,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_gee.mk
 endif
-ifeq (pa_geeb,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_geeb.mk
+ifeq (venture_geeb,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_geeb.mk
 endif
-ifeq (pa_hercules,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_hercules.mk
+ifeq (venture_hercules,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_hercules.mk
 endif
-ifeq (pa_hikari,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_hikari.mk
+ifeq (venture_hikari,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_hikari.mk
 endif
-ifeq (pa_hlte,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_hlte.mk
+ifeq (venture_hlte,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_hlte.mk
 endif
-ifeq (pa_honami,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_honami.mk
+ifeq (venture_honami,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_honami.mk
 endif
-ifeq (pa_i605,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_i605.mk
+ifeq (venture_i605,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_i605.mk
 endif
-ifeq (pa_i9082,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_i9082.mk
+ifeq (venture_i9082,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_i9082.mk
 endif
-ifeq (pa_i9100,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_i9100.mk
+ifeq (venture_i9100,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_i9100.mk
 endif
-ifeq (pa_i9100g,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_i9100g.mk
+ifeq (venture_i9100g,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_i9100g.mk
 endif
-ifeq (pa_i9300,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_i9300.mk
+ifeq (venture_i9300,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_i9300.mk
 endif
-ifeq (pa_i9500,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_i9500.mk
+ifeq (venture_i9500,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_i9500.mk
 endif
-ifeq (pa_jem,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_jem.mk
+ifeq (venture_jem,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_jem.mk
 endif
-ifeq (pa_jemlte,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_jemlte.mk
+ifeq (venture_jemlte,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_jemlte.mk
 endif
-ifeq (pa_jflte,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_jflte.mk
+ifeq (venture_jflte,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_jflte.mk
 endif
-ifeq (pa_l900,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_l900.mk
+ifeq (venture_l900,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_l900.mk
 endif
-ifeq (pa_ls980,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_ls980.mk
+ifeq (venture_ls980,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_ls980.mk
 endif
-ifeq (pa_m4,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_m4.mk
+ifeq (venture_m4,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_m4.mk
 endif
-ifeq (pa_m7,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_m7.mk
+ifeq (venture_m7,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_m7.mk
 endif
-ifeq (pa_m7spr,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_m7spr.mk
+ifeq (venture_m7spr,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_m7spr.mk
 endif
-ifeq (pa_m7vzw,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_m7vzw.mk
+ifeq (venture_m7vzw,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_m7vzw.mk
 endif
-ifeq (pa_m8,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_m8.mk
+ifeq (venture_m8,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_m8.mk
 endif
-ifeq (pa_maserati,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_maserati.mk
+ifeq (venture_maserati,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_maserati.mk
 endif
-ifeq (pa_melius3gxx,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_melius3gxx.mk
+ifeq (venture_melius3gxx,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_melius3gxx.mk
 endif
-ifeq (pa_meliusltexx,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_meliusltexx.mk
+ifeq (venture_meliusltexx,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_meliusltexx.mk
 endif
-ifeq (pa_mint,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_mint.mk
+ifeq (venture_mint,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_mint.mk
 endif
-ifeq (pa_moto_msm8960,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_moto_msm8960.mk
+ifeq (venture_moto_msm8960,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_moto_msm8960.mk
 endif
-ifeq (pa_moto_msm8960_jbbl,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_moto_msm8960_jbbl.mk
+ifeq (venture_moto_msm8960_jbbl,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_moto_msm8960_jbbl.mk
 endif
-ifeq (pa_moto_msm8960dt,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_moto_msm8960dt.mk
+ifeq (venture_moto_msm8960dt,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_moto_msm8960dt.mk
 endif
-ifeq (pa_n1,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_n1.mk
+ifeq (venture_n1,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_n1.mk
 endif
-ifeq (pa_n5100,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_n5100.mk
+ifeq (venture_n5100,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_n5100.mk
 endif
-ifeq (pa_n5110,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_n5110.mk
+ifeq (venture_n5110,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_n5110.mk
 endif
-ifeq (pa_n5120,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_n5120.mk
+ifeq (venture_n5120,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_n5120.mk
 endif
-ifeq (pa_n7000,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_n7000.mk
+ifeq (venture_n7000,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_n7000.mk
 endif
-ifeq (pa_n7100,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_n7100.mk
+ifeq (venture_n7100,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_n7100.mk
 endif
-ifeq (pa_nozomi,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_nozomi.mk
+ifeq (venture_nozomi,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_nozomi.mk
 endif
-ifeq (pa_odin,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_odin.mk
+ifeq (venture_odin,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_odin.mk
 endif
-ifeq (pa_p3100,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_p3100.mk
+ifeq (venture_p3100,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_p3100.mk
 endif
-ifeq (pa_p3110,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_p3110.mk
+ifeq (venture_p3110,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_p3110.mk
 endif
-ifeq (pa_p880,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_p880.mk
+ifeq (venture_p880,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_p880.mk
 endif
-ifeq (pa_serrano3gxx,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_serrano3gxx.mk
+ifeq (venture_serrano3gxx,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_serrano3gxx.mk
 endif
-ifeq (pa_serranoltexx,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_serranoltexx.mk
+ifeq (venture_serranoltexx,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_serranoltexx.mk
 endif
-ifeq (pa_sirius,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_sirius.mk
+ifeq (venture_sirius,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_sirius.mk
 endif
-ifeq (pa_skyrocket,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_skyrocket.mk
+ifeq (venture_skyrocket,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_skyrocket.mk
 endif
-ifeq (pa_spyder,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_spyder.mk
+ifeq (venture_spyder,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_spyder.mk
 endif
-ifeq (pa_targa,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_targa.mk
+ifeq (venture_targa,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_targa.mk
 endif
-ifeq (pa_tate,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_tate.mk
+ifeq (venture_tate,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_tate.mk
 endif
-ifeq (pa_tf201,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_tf201.mk
+ifeq (venture_tf201,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_tf201.mk
 endif
-ifeq (pa_togari,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_togari.mk
+ifeq (venture_togari,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_togari.mk
 endif
-ifeq (pa_umts_spyder,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_umts_spyder.mk
+ifeq (venture_umts_spyder,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_umts_spyder.mk
 endif
-ifeq (pa_v500,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_v500.mk
+ifeq (venture_v500,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_v500.mk
 endif
-ifeq (pa_ville,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_ville.mk
+ifeq (venture_ville,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_ville.mk
 endif
-ifeq (pa_vs980,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_vs980.mk
+ifeq (venture_vs980,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_vs980.mk
 endif
-ifeq (pa_yuga,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_yuga.mk
+ifeq (venture_yuga,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/venture_yuga.mk
 endif

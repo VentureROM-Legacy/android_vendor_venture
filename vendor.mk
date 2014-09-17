b8b5432a3fc824513d6a29775a6c2a7cc0fd3111
Vedant Mathur <vedant@mathur.ca>
2014-09-16 20:34:52 -0700

    0.20 A1
    It'll be once I finish everything haha

--- vendor.mk
+++ vendor.mk
@@ -3,10 +3,10 @@ export VENDOR := venture
 
 # Version info
 ROM_VERSION_MAJOR := 0
-ROM_VERSION_MINOR := 12
+ROM_VERSION_MINOR := 20
 ROM_VERSION_MAINTENANCE := 
 # B# Beta
 # A# Alpha
 # T# Test
 # R# Release
-ROM_VERSION_TAG := A2
+ROM_VERSION_TAG := A1

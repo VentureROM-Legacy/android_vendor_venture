1212b654b149b54d159cb338b5ea861dd7b9396f
Topazstix <robertdrone@ovi.com>
2014-10-09 17:52:19 -0600

    changing AOSPA in build to Venture

--- build.sh
+++ build.sh
@@ -63,7 +63,7 @@ fi
 # Get start time
 res1=$(date +%s.%N)
 
-echo -e "${cya}Building ${bldcya}AOSPA $VERSION for $DEVICE ${txtrst}";
+echo -e "${cya}Building ${bldcya}Venture $VERSION for $DEVICE ${txtrst}";
 echo -e "${bldgrn}Start time: $(date) ${txtrst}"
 
 # Decide what command to execute

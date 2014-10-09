47cf405b3878429e77b342286453b3ffc867738e
RobertDrone <robertdrone@ovi.com>
2014-10-09 16:26:12 -0600

    Shaken, not stirred.

--- vendor.mk
+++ vendor.mk
@@ -3,11 +3,11 @@ export VENDOR := venture
 
 # Version info
 ROM_VERSION_MAJOR := 0
-ROM_VERSION_MINOR := 20
+ROM_VERSION_MINOR := 07
 ROM_VERSION_MAINTENANCE := 
 # B# Beta
 # A# Alpha
 # T# Test
 # R# Release
 # EXP# Experimental/test version. WILL DESTROY KITTUHNS
-ROM_VERSION_TAG := A1
+ROM_VERSION_TAG := Bond

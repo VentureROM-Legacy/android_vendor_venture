72bef3d508260a4b3f3dfeec1be70e31c4960936
Topazstix <robertdrone@ovi.com>
2014-10-17 20:26:04 -0600

    revert to vedants 'R' the pirate

--- vendor.mk
+++ vendor.mk
@@ -1,13 +1,8 @@
 # Export rom name
 export VENDOR := venture
-
 # Version info
 ROM_VERSION_MAJOR := 0
-ROM_VERSION_MINOR := 24
-ROM_VERSION_MAINTENANCE := 
-# B# Beta
-# A# Alpha
-# T# Test
-# R# Release
-# EXP# Experimental/test version. WILL DESTROY KITTUHNS
-ROM_VERSION_TAG := A4
+ROM_VERSION_MINOR := 23
+ROM_VERSION_MAINTENANCE :=
+# R Revision WILL DESTROY KITTUHNS
+ROM_VERSION_TAG := R4

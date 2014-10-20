dc7834efacf64e26fb3177997e98244983b98a42
RobertDrone <robertdrone@ovi.com>
2014-10-19 23:29:49 -0600

    adding details
    commit striped from upstream venture change

--- vendor.mk
+++ vendor.mk
@@ -2,7 +2,10 @@
 export VENDOR := venture
 # Version info
 ROM_VERSION_MAJOR := 0
-ROM_VERSION_MINOR := 23
+ROM_VERSION_MINOR := 24
 ROM_VERSION_MAINTENANCE :=
 # R Revision WILL DESTROY KITTUHNS
-ROM_VERSION_TAG := R4
+ROM_VERSION_TAG := R1
+# Increment MAJOR by 1 for every weekly release
+# Increment MINOR by 1 every time you starty working on a new aspect and/or something new is added
+# Increment TAG by 1 for every revision of code modification youa re doing for an aspect and/or feature
